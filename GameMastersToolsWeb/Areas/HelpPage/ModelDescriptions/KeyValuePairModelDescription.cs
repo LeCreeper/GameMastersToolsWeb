f45f89d10cab038fd8dd3cbbd2eef07c01fbd2c7
Rasmus Drotved Laursen <rasm781y@edu.easj.dk>
2019-05-07 11:16:17 +0200

    Add project files.

---
+++ GameMastersToolsWeb/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace GameMastersToolsWeb.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
